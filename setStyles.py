e462045d3d8530fe8fbf2532be96378061ece56c
Amrit Dubey <amritdubey22003@gmail.com>
2025-05-08 19:42:44 +0530

    CSS changes

--- setStyles.py
+++ setStyles.py
@@ -11,6 +11,36 @@ def set_styles(jpg_file):
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
+        color: #fff;
+    }}
+
+    textarea {{
+        background-color: rgb(19, 23, 32) !important; 
+        border: 1px solid #444 !important;
+        border-radius: 6px !important;
+        color: #fff !important;
+    }}
+
+    .stTextArea label {{
+        color: #fff !important;
+    }}
+
+    .stTextArea div[data-testid="InputInstructions"]{{
+        color: #fff !important;
+    }}
+
+    .stFileUploader label, .stFileUploader small{{
+        color: #fff !important;
+    }}
+
+    .stFileUploader button[data-testid="stBaseButton-secondary"]{{
+        background-color: rgb(38, 39, 48) !important;
+        color: #fff !important;
+    }}
+
+    .stFileUploader section[data-testid="stFileUploaderDropzone"]{{
+        background-color: rgb(19, 23, 32) !important;
+        color: #fff !important;
     }}
 
     .stButton > button:disabled {{
@@ -22,6 +52,31 @@ def set_styles(jpg_file):
     .stButton > button {{
         background: rgb(19, 23, 32);
         color: white;
+    }}
+
+    .stMarkdown div[data-testid="stMarkdownContainer"] > p > code{{
+        background: rgb(19, 23, 32);
+    }}
+
+     hr {{
+        background-color: #fff !important;
+    }}
+
+    section[data-testid="stSidebar"] {{
+        background-color: #121212 !important;  
+        color: white; 
+    }}
+    
+    section[data-testid="stSidebar"] h1,
+    section[data-testid="stSidebar"] h2,
+    section[data-testid="stSidebar"] h3,
+    section[data-testid="stSidebar"] h4,
+    section[data-testid="stSidebar"] h5,
+    section[data-testid="stSidebar"] h6,
+    div[data-testid="stSidebarCollapseButton"],
+    section[data-testid="stSidebar"] label,
+    section[data-testid="stSidebar"] span, .stRadio p{{
+        color: white !important;
     }}
     </style>
     """
